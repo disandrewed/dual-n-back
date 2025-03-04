3fea065b78ba434f0517a70286a28ecccc5484c5
Andrew Tang <disandrewed@gmail.com>
2025-03-04 21:51:21 +1100

    updated to allow analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import DualNBackGame from './DualNBackGame';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App">
       <DualNBackGame />
+      <Analytics />
     </div>
   );
 }
